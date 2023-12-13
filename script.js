fc8c4a54e348f9cc04bfd49818eab7f66de9903e
uncanny-vegan-taco <kylihumphreys@gmail.com>
2023-12-13 15:58:28 -0800

    Added a JS file
    I intend to have the information automatically formatted and added to a prepped email to me

---
+++ script.js
@@ -0,0 +1,4 @@
+let email = "mailto:kyliwhite@perkinscoie.com";
+const buttonElement = document.querySelector("button");
+buttonElement.addEventListener("submit", () => {});
+//Send Form Info in email
